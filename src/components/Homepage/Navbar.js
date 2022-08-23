067c1659ef8cd18608a48c49e8161bf0c59b163f
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-23 13:22:45 +0200

    Adding Google font import link

--- src/components/Homepage/Navbar.js
+++ src/components/Homepage/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from "styled-components"
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
 
 function Navbar() {
   return (
     <Wrapper>
-       
+
     </Wrapper>
   );
 }
 
 
 export const Wrapper = styled.div`
-  
+  background-color: #FFFFFF;
+  width: 980px;
+  height: 70px;
+  border: 3px solid red;
 `
 
 export default Navbar;
