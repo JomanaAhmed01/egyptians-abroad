137112036b8e637af57b76304938374c29f8cc36
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-25 20:38:15 +0200

    Defining AskAnything.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/Homepage/Navbar'
 import ConnectEgyptians from '../components/Homepage/ConnectEgyptains'
+import AskAnything from '../components/Homepage/AskAnything'
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <ConnectEgyptians />
+      <AskAnything />
     </Wrapper>
   );
 }
