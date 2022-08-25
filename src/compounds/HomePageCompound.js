a9d200912a859634377228d75fc731eaf141d911
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-25 15:52:33 +0200

    Defining ConnectEgyptians.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
-import Navbar from '../components/Homepage/Navbar';
+import Navbar from '../components/Homepage/Navbar'
+import ConnectEgyptians from '../components/Homepage/ConnectEgyptains'
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <ConnectEgyptians />
     </Wrapper>
   );
 }
