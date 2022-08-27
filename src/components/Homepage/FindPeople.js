39fbee09e3ef081861fed6da12b8022d63badf71
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-27 14:38:41 +0200

    Creating FindPeople.js file

---
+++ src/components/Homepage/FindPeople.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
+
+function FindPeople() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default FindPeople;
