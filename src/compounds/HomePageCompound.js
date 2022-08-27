c47d0d65365be56b00dc2c1a2106dd7b5efe51c1
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-27 14:39:20 +0200

    Defining FindPeople.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import Navbar from '../components/Homepage/Navbar'
 import ConnectEgyptians from '../components/Homepage/ConnectEgyptains'
 import AskAnything from '../components/Homepage/AskAnything'
+import FindPeople from '../components/Homepage/FindPeople'
 
 function HomePageCompound() {
   return (
@@ -10,6 +11,7 @@ function HomePageCompound() {
       <Navbar />
       <ConnectEgyptians />
       <AskAnything />
+      <FindPeople />
     </Wrapper>
   );
 }
