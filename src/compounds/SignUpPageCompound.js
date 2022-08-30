1ea61bd90ebaf7c05249bd81b7222ad2df7e570d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-30 19:52:19 +0200

    Adding Footer section to the Sign up page

--- src/compounds/SignUpPageCompound.js
+++ src/compounds/SignUpPageCompound.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from "styled-components"
 import Logo from '../components/SignUpPage/Logo'
+import SignUpCard from '../components/SignUpPage/SignUpCard'
+import Footer from '../components/Homepage/Footer'
 
 function SignUpPageCompound() {
   return (
     <Wrapper>
       <Logo />
+      <SignUpCard />
+      <Footer />
     </Wrapper>
   );
 }
