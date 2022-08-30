f3dfb2f63beeff7c296a0942879b69ba4c79b669
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-30 19:39:49 +0200

    Creating SignUpPageCompound.js file

---
+++ src/compounds/SignUpPageCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function SignUpPageCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default SignUpPageCompound;
