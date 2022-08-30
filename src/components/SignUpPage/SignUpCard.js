228d5ce3ab707b396944728d0d8ba32c6f5c5f21
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-30 19:49:12 +0200

    Creating SignUpCard.js file

---
+++ src/components/SignUpPage/SignUpCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
+
+function SignUpCard() {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default SignUpCard;
