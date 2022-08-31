1f4c8375ab2008f9831502f6f7b799c72829d4ad
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 13:34:05 +0200

    Defining LogInCard.js file in the LogInPageCompound.js file

--- src/compounds/LogInPageCompound.js
+++ src/compounds/LogInPageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import Logo from '../components/LogInPage/Logo'
+import LogInCard from '../components/LogInPage/LogInCard'
 
 function LogInPageCompound() {
   return (
     <Wrapper>
       <Logo />
+      <LogInCard />
     </Wrapper>
   );
 }
