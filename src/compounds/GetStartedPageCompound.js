d9453875599196fee8252562f9216a97ec589706
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 18:31:17 +0200

    Creating GetStartedPageCompound.js file

---
+++ src/compounds/GetStartedPageCompound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components"
+
+function GetStartedPageCompound() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default GetStartedPageCompound;
