76852f66dab1958a9c8f653287aba09e2b7946f6
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 18:50:19 +0200

    Defining Header.js file in the GetStartedPageCompound.js file

--- src/compounds/GetStartedPageCompound.js
+++ src/compounds/GetStartedPageCompound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components"
+import Header from '../components/GetStarted/Header'
 
 function GetStartedPageCompound() {
     return (
         <Wrapper>
-
+          <Header />
         </Wrapper>
     );
 }
