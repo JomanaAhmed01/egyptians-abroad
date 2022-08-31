e0b263a2540ddb68123833d8976da729a3812a49
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 13:49:28 +0200

    Adding Footer to the Log in page

--- src/compounds/LogInPageCompound.js
+++ src/compounds/LogInPageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Logo from '../components/LogInPage/Logo'
 import LogInCard from '../components/LogInPage/LogInCard'
+import Footer from '../components/Homepage/Footer'
 
 function LogInPageCompound() {
   return (
     <Wrapper>
       <Logo />
       <LogInCard />
+      <Footer />
     </Wrapper>
   );
 }
