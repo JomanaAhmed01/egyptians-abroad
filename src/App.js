3e7284ae1b1925c82f378206644140997dc5ac8a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 13:28:11 +0200

    Hiding SignUpPageCompound.js file from the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 // import HomePageCompound from './compounds/HomePageCompound'
-import SignUpPageCompound from './compounds/SignUpPageCompound'
+// import SignUpPageCompound from './compounds/SignUpPageCompound'
 
 function App() {
   return (
     <div className="App">
       {/* <HomePageCompound /> */}
-      <SignUpPageCompound />
+      {/* <SignUpPageCompound /> */}
     </div>
   );
 }
