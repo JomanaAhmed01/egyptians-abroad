56c40d732b268a4b08b2f86376748a2bd54468e4
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 19:05:59 +0200

    Defining Profile.js file in the GetStartedPageCompound.js file

--- src/compounds/GetStartedPageCompound.js
+++ src/compounds/GetStartedPageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import Header from '../components/GetStarted/Header'
+import Profile from '../components/GetStarted/Profile'
 
 function GetStartedPageCompound() {
   return (
     <Wrapper>
       <Header />
+      <Profile />
     </Wrapper>
   );
 }
