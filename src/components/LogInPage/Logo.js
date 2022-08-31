691d46528b8241425173ae68cd1f1d990a24fc46
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-31 13:31:19 +0200

    Creating Egyptians abroad project's Login page's Logo section

---
+++ src/components/LogInPage/Logo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
+
+function Logo() {
+  return (
+    <Wrapper>
+      <Header>EgyptiansAbroad</Header>
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Header = styled.p`
+  color: #1399FF;
+  font-family: 'Caveat', cursive;
+  font-size: 40px;
+  cursor: pointer;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 25px;
+  } 
+`
+
+export default Logo;
