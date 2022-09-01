f2f87dc44f2b4d8405908c9f704efdc9f24ea99a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 14:18:27 +0200

    Defining AllPostsPageCompound.js file in the App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import SignUpPageCompound from './compounds/SignUpPageCompound'
 // import LogInPageCompound from './compounds/LogInPageCompound'
 // import GetStartedPageCompound from './compounds/GetStartedPageCompound'
+import AllPostsPageCompound from './compounds/AllPostsPageCompound'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/* <SignUpPageCompound /> */}
       {/* <LogInPageCompound /> */}
       {/* <GetStartedPageCompound /> */}
+      <AllPostsPageCompound />
     </div>
   );
 }
