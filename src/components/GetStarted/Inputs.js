a4ac35bec10875bb8b3dd78a1340b267c21aff03
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 12:32:24 +0200

    Creating Inputs.js file

---
+++ src/components/GetStarted/Inputs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
+
+function Inputs() {
+  return (
+    <Wrapper>
+      <InputAndLabelWrapper>
+        
+      </InputAndLabelWrapper>
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export const InputAndLabelWrapper = styled.div`
+  
+`
+
+export default Inputs;
