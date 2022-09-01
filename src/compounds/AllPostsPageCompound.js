6140763514a8947973a2f8bf6446754a1c2163ce
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 14:21:31 +0200

    Defining Navbar.js file in the AllPostsPageCompound.js file

--- src/compounds/AllPostsPageCompound.js
+++ src/compounds/AllPostsPageCompound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components"
+import Navbar from '../components/AllPostsPage/Navbar'
 
 function AllPostsPageCompound() {
     return (
         <Wrapper>
-
+          <Navbar />
         </Wrapper>
     );
 }
