1a5ce67edb90b7090f5e6be606d940022ee81419
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 14:49:14 +0200

    Defining Header.js file in the AllPostsPageCompound.js file

--- src/compounds/AllPostsPageCompound.js
+++ src/compounds/AllPostsPageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/AllPostsPage/Navbar'
+import Header from '../components/AllPostsPage/Header'
 
 function AllPostsPageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Header />
     </Wrapper>
   );
 }
