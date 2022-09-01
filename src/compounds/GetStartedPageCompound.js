12e2787c9621bbc2b89c69be5f62102b2e239742
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 12:32:42 +0200

    Defining Inputs.js file in the GetStartedPageCompound.js file

--- src/compounds/GetStartedPageCompound.js
+++ src/compounds/GetStartedPageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Header from '../components/GetStarted/Header'
 import Profile from '../components/GetStarted/Profile'
+import Inputs from '../components/GetStarted/Inputs'
 
 function GetStartedPageCompound() {
   return (
     <Wrapper>
       <Header />
       <Profile />
+      <Inputs />
     </Wrapper>
   );
 }
