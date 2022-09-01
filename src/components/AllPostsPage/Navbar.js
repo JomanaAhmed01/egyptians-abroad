5a406a2c64b6e82337565236c238b0f4bef76f90
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-01 14:41:18 +0200

    Creating Egyptains abroad project's All posts page's Navbar section

--- src/components/AllPostsPage/Navbar.js
+++ src/components/AllPostsPage/Navbar.js
@@ -7,14 +7,71 @@ import styled from "styled-components"
 function Navbar() {
     return (
         <Wrapper>
+            <LogoWrapper>
+                <Logo>EgyptainsAbroad</Logo>
+            </LogoWrapper>
 
+            <UserWrapper>
+                <Username>ojy_ahmed01</Username>
+                <ProfileIcon src='./img/avatar.png' />
+            </UserWrapper>
         </Wrapper>
     );
 }
 
 
 export const Wrapper = styled.div`
+  background-color: #FFFFFFCC;
+  width: 980px;
+  height: 70px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  border-radius: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  @media screen and (max-width: 1024px) {
+    width: 95%;
+    justify-content: space-between;
+  }
+`
+
+export const LogoWrapper = styled.div`
   
+`
+
+export const Logo = styled.p`
+  color: #1399FF;
+  font-family: 'Caveat', cursive;
+  font-size: 25px;
+  cursor: pointer;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 20px;
+  }
+`
+
+export const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Username = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  color: rgb(54, 54, 54);
+  margin-right: 15px;
+`
+
+export const ProfileIcon = styled.img`
+  width: 42px;
+  height: 42px;
+  cursor: pointer;
+  border-radius: 50%;
 `
 
 export default Navbar;
