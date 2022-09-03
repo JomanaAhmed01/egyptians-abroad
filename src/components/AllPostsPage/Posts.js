8cd1f83d230ead26c1427db927ae5343d9af7938
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-03 17:11:43 +0200

    Creating Posts.js file

---
+++ src/components/AllPostsPage/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+</style>
+
+function Posts() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default Posts;
