4dd8755f752aa2be5108caf151515f7808df943d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-03 17:12:38 +0200

    Defining Posts.js file in the AllPostsPageCompound.js file

--- src/compounds/AllPostsPageCompound.js
+++ src/compounds/AllPostsPageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/AllPostsPage/Navbar'
 import Header from '../components/AllPostsPage/Header'
+import Posts from '../components/AllPostsPage/Posts'
 
 function AllPostsPageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Header />
+      <Posts />
     </Wrapper>
   );
 }
