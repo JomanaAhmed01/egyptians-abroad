e81235fe28aebd7860d36ac6c0198a58c656ece9
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-04 20:17:27 +0200

    Creating the Menu section that appears when clicking on the Username section in the Navbar

--- src/components/AllPostsPage/Navbar.js
+++ src/components/AllPostsPage/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from "styled-components"
+import { PersonCircle } from '@styled-icons/bootstrap/PersonCircle'
+import { Edit } from '@styled-icons/boxicons-solid/Edit'
+import { LogOut } from '@styled-icons/entypo/LogOut'
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
 </style>
@@ -11,10 +14,29 @@ function Navbar() {
         <Logo>EgyptainsAbroad</Logo>
       </LogoWrapper>
 
-      <UserWrapper>
-        <Username>ojy_ahmed01</Username>
-        <ProfileIcon src='./img/avatar.png' />
-      </UserWrapper>
+      <UserAndMenuWrapper>
+        <UserWrapper>
+          <Username>ojy_ahmed01</Username>
+          <ProfileIcon src='./img/avatar.png' />
+        </UserWrapper>
+
+        <MenuWrapper>
+          <ItemWrapper>
+            <Profile />
+            <Item>My Profile</Item>
+          </ItemWrapper>
+
+          <ItemWrapper>
+            <Change />
+            <Item>Edit Profile</Item>
+          </ItemWrapper>
+
+          <ItemWrapper>
+            <Logout />
+            <Item>Logout</Item>
+          </ItemWrapper>
+        </MenuWrapper>
+      </UserAndMenuWrapper>
     </Wrapper>
   );
 }
@@ -73,6 +95,52 @@ export const ProfileIcon = styled.img`
   height: 42px;
   cursor: pointer;
   border-radius: 50%;
+`
+
+export const UserAndMenuWrapper = styled.div`
+  margin-bottom: -170px;
+`
+
+export const MenuWrapper = styled.div`
+  background: #FFFFFF;
+  border-radius: 22.5px;
+  box-shadow: rgb(0 0 0 / 5%) 0px 13px 20px 0px;
+  width: 175px;
+  height: 150px;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`
+
+export const Profile = styled(PersonCircle)`
+  width: 15px;
+  height: 15px;
+`
+
+export const Change = styled(Edit)`
+  width: 15px;
+  height: 15px;
+`
+
+export const Logout = styled(LogOut)`
+  width: 15px;
+  height: 15px;
+  margin-left: -20px;
+`
+
+export const Item = styled.p`
+  color: #000000;
+  font-size: 16px;
+  font-weight: 600;
+  margin-left: 15px;
 `
 
 export default Navbar;
