be4ed8ac169f6a4ce8ca44889368e08800c4e644
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-07 19:57:47 +0200

    Displaying the Home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-// import HomePageCompound from './compounds/HomePageCompound'
+import HomePageCompound from './compounds/HomePageCompound'
 // import SignUpPageCompound from './compounds/SignUpPageCompound'
 // import LogInPageCompound from './compounds/LogInPageCompound'
 // import GetStartedPageCompound from './compounds/GetStartedPageCompound'
-import AllPostsPageCompound from './compounds/AllPostsPageCompound'
+// import AllPostsPageCompound from './compounds/AllPostsPageCompound'
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePageCompound /> */}
+      <HomePageCompound />
       {/* <SignUpPageCompound /> */}
       {/* <LogInPageCompound /> */}
       {/* <GetStartedPageCompound /> */}
-      <AllPostsPageCompound />
+      {/* <AllPostsPageCompound /> */}
     </div>
   );
 }
