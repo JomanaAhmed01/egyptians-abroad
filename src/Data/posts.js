b69bea94534200c161f1e4cd5b013f02f2974a0b
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-07 19:38:03 +0200

    Creating posts.js file

---
+++ src/Data/posts.js
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+        id: 1,
+        name: "post",
+        title: 'How to build a Mobile game?',
+    },
+    {
+        id: 2,
+        name: "post",
+        title: 'My first post - Hala',
+    },
+    {
+        id: 3,
+        name: "post",
+        title: 'UK study Visa',
+    },
+]
