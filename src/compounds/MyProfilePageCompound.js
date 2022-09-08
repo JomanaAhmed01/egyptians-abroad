8defe96671dece13d7803df4b5cc895c55b28736
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-08 20:37:28 +0200

    Defining Navbar.js file in the MyProfilePageCompound.js file

---
+++ src/compounds/MyProfilePageCompound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components"
+import Navbar from '../components/MyProfilePage/Navbar'
+
+function MyProfilePageCompound() {
+    return (
+        <Wrapper>
+          <Navbar />
+        </Wrapper>
+    );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default MyProfilePageCompound;
