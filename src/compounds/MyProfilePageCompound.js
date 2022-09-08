f55d31cbaf5eb753eee50ef110c970605ff7c5cb
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-08 21:02:46 +0200

    Defining HeaderAndButton.js file in the MyProfilePageCompound.js file

--- src/compounds/MyProfilePageCompound.js
+++ src/compounds/MyProfilePageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/MyProfilePage/Navbar'
 import Header from '../components/MyProfilePage/Header'
+import HeaderAndButton from '../components/MyProfilePage/HeaderAndButton'
 
 function MyProfilePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Header />
+      <HeaderAndButton />
     </Wrapper>
   );
 }
